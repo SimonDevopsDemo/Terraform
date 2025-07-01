e218f640ab3dbab63ffe9f96fa870202b11a31c3
SimonDevopsDemo <simonabraham710@gmail.com>
2025-07-01 17:44:10 +0530

    new files added

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id        = "ami-0d03cb826412c6b0f" # Replace with valid AMI
+key_name      = "simon"
+instance_name = "jenkins-provisioned-ec2"
---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the existing key pair"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+}
