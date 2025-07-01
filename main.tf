971539b8af5b023dcdef8cc0196fe923b7154897
SimonDevopsDemo <simonabraham710@gmail.com>
2025-07-01 18:02:36 +0530

    changed key pair

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
 
   tags = {
     Name = var.instance_name
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,2 @@
 ami_id        = "ami-0d03cb826412c6b0f" # Replace with valid AMI
-key_name      = "simon"
 instance_name = "jenkins-provisioned-ec2"
