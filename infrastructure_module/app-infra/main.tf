30de26323489deb869bddd08d0832d837580d6c9
SimonDevopsDemo <simonabraham710@gmail.com>
2025-07-02 16:13:47 +0530

    great changes modified

--- infrastructure_module/app-infra/main.tf
+++ infrastructure_module/app-infra/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source             = "../modules/vpc"
+  source             = "../resource_modules/vpc"
   aws_region         = var.aws_region
   vpc_cidr           = var.vpc_config.cidr_block
   public_subnet_cidr = var.vpc_config.subnet_cidr_block
 }
 
 module "ec2_instance" {
-  source         = "../modules/ec2"
+  source         = "../resource_modules/ec2"
   subnet_id      = module.vpc.public_subnet_id
   instance_type  = var.ec2_config.instance_type
   ami_id         = var.ec2_config.ami_id
